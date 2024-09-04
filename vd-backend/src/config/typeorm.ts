3179d5a9160f29c5764733e7d440d5ec447b1fa9
Adam Rosołowski <aa.rosolowski@gmail.com>
2024-09-04 18:03:01 +0200

    fix

--- vd-backend/src/config/typeorm.ts
+++ vd-backend/src/config/typeorm.ts
@@ -14,7 +14,7 @@ const config = {
   entities: ['dist/**/*.entity{.ts,.js}'],
   migrations: ['dist/migrations/*{.ts,.js}'],
   autoLoadEntities: true,
-  synchronize: process.env.NODE_ENV !== 'production',
+  synchronize: true,
 };
 
 export default registerAs('typeorm', () => config);
