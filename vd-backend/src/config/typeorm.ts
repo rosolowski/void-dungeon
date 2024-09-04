3769e2ff28fd2543dde76579c888d07c0bc4666c
Adam Rosołowski <aa.rosolowski@gmail.com>
2024-09-04 17:13:18 +0200

    fix

--- vd-backend/src/config/typeorm.ts
+++ vd-backend/src/config/typeorm.ts
@@ -1,5 +1,4 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
-
 import { config as dotenvConfig } from 'dotenv';
 import { registerAs } from '@nestjs/config';
 
@@ -7,15 +6,15 @@ dotenvConfig({ path: '.env' });
 
 const config = {
   type: 'postgres',
-  host: `${process.env.DB_HOST}`,
-  port: `${process.env.DB_PORT}`,
-  username: `${process.env.DB_USERNAME}`,
-  password: `${process.env.DB_PASSWORD}`,
-  database: `${process.env.DB_NAME}`,
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT, 10) || 5432,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   entities: ['dist/**/*.entity{.ts,.js}'],
   migrations: ['dist/migrations/*{.ts,.js}'],
   autoLoadEntities: true,
-  synchronize: true,
+  synchronize: process.env.NODE_ENV !== 'production',
 };
 
 export default registerAs('typeorm', () => config);
